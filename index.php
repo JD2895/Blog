461796d7176a60492dff131198dc490e89834aa8
JD2895 <j.davis.2895@gmail.com>
2018-10-22 16:44:10 -0400

    Initial commit
    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Base</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<header>
+	
+</header>
+	<nav>
+		<div class="main-wrapper">
+			
+		</div>
+	</nav>
+<section>
+
+</section>
+
+</body>
+</html>
