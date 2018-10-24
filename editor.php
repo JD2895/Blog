3495bbfa24834db257d66ca4acbd17abacf1cdd8
JD2895 <j.davis.2895@gmail.com>
2018-10-24 15:33:57 -0400

    Added CKEditor
    Added ckeditor

---
+++ editor.php
@@ -0,0 +1,51 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Base</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="ckeditor/ckeditor.js"></script>
+</head>
+<body>
+
+<header>
+	<nav>
+		<div class="main-wrapper">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+			</ul>
+			<div class="nav-login">
+				<?php
+					if (isset($_SESSION['u_id'])){
+						echo '
+						<form action="includes/logout.inc.php" method="POST">
+							<button type="submit" name="submit">Logout</button>
+						</form>';
+					} else {
+						echo '
+						<form action="includes/login.inc.php" method="POST">
+							<input type="text" name="uid" placeholder="Username/e-mail">
+							<input type="password" name="pwd" placeholder="password">
+							<button type="submit" name="submit">Login</button>
+						</form>
+						<a href="signup.php">Sign up</a>
+						';
+					}
+				?>
+			</div>
+		</div>
+	</nav>
+</header>
+
+<textarea class="ckeditor" name="editor"></textarea>
+
+<?php
+	include_once 'footer.php';
+?>
