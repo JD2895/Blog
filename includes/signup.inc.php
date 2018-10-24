95ec2a7763bd7193e71542e94b3fa578825cc2a6
JD2895 <j.davis.2895@gmail.com>
2018-10-24 13:36:35 -0400

    Update includes/signup.inc.php
    test2

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -14,8 +14,21 @@ if (isset($_POST['submit'])){
 		header("Location: ../signup.php?signup=empty");
 		exit();
 	} else {
-					header("Location: ../phpdebug.php");
 		
+		//Check if input characters are valid
+		if(!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last)){
+			header("Location: ../signup.php?signup=invalid");
+			exit();
+		} else {
+			
+			//Check if email is valid
+			if (filter_val($email, FILTER_VALIDATE_EMAIL)){
+				header("Location: ../signup.php?signup=email");
+				exit();
+			} else {
+					header("Location: ../phpdebug.php");
+			}
+		}
 	}
 	
 } else {
