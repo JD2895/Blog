523b076d6a988c6cbc2becf8f8456e00a243bd54
JD2895 <j.davis.2895@gmail.com>
2018-10-24 00:15:33 -0400

    added header footer
    added header footer

--- index.php
+++ index.php
@@ -1,34 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Base</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
+<?php
+	include_once 'header.php';
+?>
 
-<header>
-	
-</header>
-	<nav>
-		<div class="main-wrapper">
-			<ul>
-				<li><a href="index.php">Home</a></li>
-			</ul>
-			<div class="nav-login">
-				<form>
-					<input type="text" name="uid" placeholder="Username/e-mail">
-					<input type="password" name="pwd" placeholder="password">
-					<button type="submit" name="submit">Login</button>
-				</form>
-				<a href="signup.php">Sign up</a>
-			</div>
-		</div>
-	</nav>
-<section>
-
+<section class="main-container">
+	<div class="main-wrapper">
+		<h2>Home</h2>
+	</div>
 </section>
 
-</body>
-</html>
+<?php
+	include_once 'footer.php';
+?>
