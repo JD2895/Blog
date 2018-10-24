3738a93275e75694481dee612ff41cfbdcef0ee0
JD2895 <j.davis.2895@gmail.com>
2018-10-24 14:05:54 -0400

    Added login
    added login

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -40,7 +40,7 @@ if (isset($_POST['submit'])){
 					//Insert the user into the database
 					$sql = "INSERT INTO users (user_first_name, user_last_name, user_email, user_uid, user_pwd) VALUES ('$first', '$last', '$email', '$uid', '$hashedPwd');";
 					$result = mysqli_query($conn, $sql);
-					header("Location: ../phpdebug.php");
+					header("Location: ../signup.php?signup=success");
 					exit();
 				}
 			}
