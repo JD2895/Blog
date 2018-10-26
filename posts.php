263262839fd778507255852b656937420fc042d5
JD2895 <j.davis.2895@gmail.com>
2018-10-25 23:19:53 -0400

    Update posts.php
    auth editing

--- posts.php
+++ posts.php
@@ -5,29 +5,34 @@
 ?>
 
 <h1> Blog posts </h1>
-<table style="border:2px solid; padding:10px;">
-	<?php
-
-		$sql = "SELECT post_id, uid, title, date_time FROM posts";
-		$result = mysqli_query($conn, $sql);
-		
-		if ($result == 0){					
-			echo 'no results';
-			exit();
-		}
-		
-		while($row = mysqli_fetch_array($result)) {
-		?>
-			<tr>
-				<td><?= $row['uid']; ?></td>
-				<td><?= $row['title']; ?></td>
-				<td><?= $row['date_time']; ?></td>
-				<td><a href="<?= 'update.php?id='.$row['post_id'] ?>">Edit</a></td>
-				<td><a href="<?= 'delete.php?id='.$row['post_id'] ?>">Delete</a></td>
-			</tr>
-		<?php 
-		}
+<table style="border:2px solid;">
+<?php
+	$sql = "SELECT post_id, uid, title, date_time FROM posts";
+	$result = mysqli_query($conn, $sql);
+	
+	if ($result == 0){					
+		echo 'no results';
+		exit();
+	}
+	
+	while($row = mysqli_fetch_array($result)) {
 	?>
+		<tr>
+			<td><?= $row['uid']; ?></td>
+			<td><?= $row['title']; ?></td>
+			<td><?= $row['date_time']; ?></td>
+			<?php 
+			if (isset($_SESSION['u_id'])){
+			?>
+				<td><a href="<?= 'update_post.php?id='.$row['post_id'] ?>">Edit</a></td>
+				<td><a href="<?= 'delete_post.php?id='.$row['post_id'] ?>">Delete</a></td>
+			<?php
+			}
+			?>
+		</tr>
+	<?php 
+	}
+?>
 </table>
 
 <?php
