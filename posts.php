d66dd6591bd93343c89aaacca39f9bf07a443d69
JD2895 <j.davis.2895@gmail.com>
2018-10-25 22:50:47 -0400

    Update posts.php
    posts test 2

--- posts.php
+++ posts.php
@@ -1,16 +1,16 @@
 <?php
 	session_start();
 	include_once 'header.php';
+	include_once 'dbblog.inc.php';
 ?>
 
 <h1> Blog posts </h1>
 <table style="border:2px solid;">
 	<?php
-		include_once '/includes/dbblog.inc.php';
-		$sql = "SELECT * FROM posts LIMIT 3;";
+		$sql = "SELECT * FROM posts";
 		$result = mysqli_query($conn, $sql);
 		
-		if ($resultCheck == 0){					
+		if ($result == 0){					
 			echo 'no results';
 			exit();
 		}
