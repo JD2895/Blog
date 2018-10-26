4a2da0f34c83fc8143ae7abd5bcbab22a66bf622
JD2895 <j.davis.2895@gmail.com>
2018-10-25 22:52:04 -0400

    Update posts.php
    posts test 3

--- posts.php
+++ posts.php
@@ -1,12 +1,23 @@
 <?php
 	session_start();
 	include_once 'header.php';
-	include_once 'dbblog.inc.php';
 ?>
 
 <h1> Blog posts </h1>
 <table style="border:2px solid;">
 	<?php
+		$dbServername = "localhost";
+		$dbUsername = "phpmyadmin";
+		$dbPassword = "phpmyadmin";
+		$dbName = "blog";
+
+		$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+		//Setting session time-zone to Toronto
+
+		$sql = "SET SESSION time_zone = '-4:00'";
+		$result = mysqli_query($conn, $sql);
+
 		$sql = "SELECT * FROM posts";
 		$result = mysqli_query($conn, $sql);
 		
