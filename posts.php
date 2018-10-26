378d0f48e4944fc67e198074350b8207f0bd246d
JD2895 <j.davis.2895@gmail.com>
2018-10-25 22:45:34 -0400

    Update posts.php
    updated include

--- posts.php
+++ posts.php
@@ -6,9 +6,14 @@
 <h1> Blog posts </h1>
 <table style="border:2px solid;">
 	<?php
-		include_once 'includes/dbblog.inc.php';
+		include_once '/includes/dbblog.inc.php';
 		$sql = "SELECT (title, date_time, uid) FROM posts LIMIT 3;";
 		$result = mysqli_query($conn, $sql);
+		
+		if ($resultCheck == 0){					
+			echo 'no results';
+			exit();
+		}
 		
 		while($row = mysqli_fetch_array($result)) {
 		?>
